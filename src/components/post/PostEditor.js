3713695f8a01fd038dc85bdac9f70ed4a886c185
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-10-29 19:37:51 +0100

    feat: update PostEditor with validation, save button, and editor config. Add error handling for title and content fields with dynamic error messages. Update Jodit editor config to disable unnecessary plugins and simplify toolbar.

--- src/components/post/PostEditor.js
+++ src/components/post/PostEditor.js
@@ -3,34 +3,68 @@ import { useMemo } from "react";
 
 const JoditEditor = dynamic(() => import("jodit-react"), { ssr: false });
 
-export default function PostEditor({ title, setTitle, content, setContent }) {
+export default function PostEditor({
+  title,
+  setTitle,
+  content,
+  setContent,
+  error,
+  onSave,
+}) {
   const editorConfig = useMemo(
     () => ({
       uploader: {
         insertImageAsBase64URI: true,
         imagesExtensions: ["jpg", "png", "jpeg", "gif", "svg", "webp"],
       },
+      showCharsCounter: false,
+      disablePlugins:
+        "clean-html,about,add-new-line,ai-assistant,backspace,class-span,drag-and-drop,drag-and-drop-element,dtd,file,focus,format-block,hotkeys,hr,iframe,image,image-processor,image-properties,indent,inline-popup,justify,key-arrow-outside,limit,line-height,media,paste-from-word,powered-by-jodit,resize-handler,resizer,search,select,select-cells,source,speech-recognize,stat,sticky,symbols,tab,table,table-keyboard-navigation,video,wrap-nodes,xpath,spellcheck,clipboard,copyformat,delete-command,color,enter,fullsize,mobile,paste-storage,placeholder,resize-cells",
+      buttons:
+        "bold,italic,underline,strikethrough,ul,ol,font,fontsize,link,undo,redo",
     }),
     []
   );
 
   return (
     <div>
+      <label htmlFor="title" className="label">
+        Post Title
+      </label>
       <input
+        id="title"
         type="text"
         placeholder="Post Title"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
         className="input input-bordered w-full"
         aria-label="Title"
-        required
       />
+      {error.title && (
+        <p className="text-error">
+          <p className="text-red-500">{error.title}</p>
+        </p>
+      )}
+
+      <label htmlFor="content" className="label mt-4">
+        Post Content
+      </label>
       <JoditEditor
+        id="content"
         value={content}
         config={editorConfig}
         onChange={(newContent) => setContent(newContent)}
         className="w-full mt-4"
       />
+      {error.content && (
+        <p className="text-error">
+          <p className="text-red-500">{error.content}</p>
+        </p>
+      )}
+
+      <button onClick={() => onSave()} className="btn btn-primary mt-4">
+        Save Post
+      </button>
     </div>
   );
 }
