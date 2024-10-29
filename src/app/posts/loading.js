521455f39c7e2a278d691c41f1353b24427e5495
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-10-29 23:56:50 +0100

    Refactor PostSkeleton component for cleaner layout and styling.

--- src/app/posts/loading.js
+++ src/app/posts/loading.js
@@ -2,32 +2,23 @@ import React from "react";
 
 const PostSkeleton = () => {
   return (
-    <div className="relative px-6 pt-16 pb-20 lg:px-8 lg:pt-24 lg:pb-28">
-      <div className="relative mx-auto max-w-7xl">
-        <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
-          <div className="bg-white flex flex-col overflow-hidden rounded-lg shadow-lg">
-            <div className="flex-shrink-0">
-              <div className="h-48 w-full bg-gray-300 animate-pulse" />
+    <div>
+      <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-xl lg:grid-cols-1 h-full w-full">
+        <div className="flex flex-col">
+          <div className="flex-shrink-0 skeleton aspect-video rounded-none"></div>
+          <div className="flex flex-1 flex-col justify-between p-6">
+            <div className="flex w-full flex-col gap-4">
+              <div className="skeleton h-4 w-28"></div>
+              <div className="skeleton h-4 w-72"></div>
+              <div className="skeleton h-4 w-full"></div>
+              <div className="skeleton h-4 w-full"></div>
             </div>
 
-            <div className="flex-1 flex flex-col justify-between p-6">
-              <div className="flex-1">
-                <div className="h-6 bg-gray-300 rounded w-3/4 mb-4 animate-pulse" />
-
-                <div className="space-y-2">
-                  <div className="h-4 bg-gray-300 rounded w-full animate-pulse" />
-                  <div className="h-4 bg-gray-300 rounded w-5/6 animate-pulse" />
-                </div>
-              </div>
-
-              <div className="mt-6 flex items-center">
-                <div className="flex-shrink-0">
-                  <div className="h-10 w-10 rounded-full bg-gray-300 animate-pulse" />
-                </div>
-                <div className="ml-3">
-                  <div className="h-4 bg-gray-300 rounded w-1/2 animate-pulse mb-2" />
-                  <div className="h-4 bg-gray-300 rounded w-1/4 animate-pulse" />
-                </div>
+            <div className="flex items-center gap-4 mt-6">
+              <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
+              <div className="flex flex-col gap-4">
+                <div className="skeleton h-4 w-20"></div>
+                <div className="skeleton h-4 w-36"></div>
               </div>
             </div>
           </div>
