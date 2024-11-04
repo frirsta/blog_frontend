4606cd378739626d2378601f95cba2dff6b2878d
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-11-04 21:50:08 +0100

    Refactor EditProfilePage to simplify picture updates

--- src/app/profile/edit/page.js
+++ src/app/profile/edit/page.js
@@ -12,11 +12,12 @@ export default function EditProfilePage() {
   const { showMessage } = useMessage();
 
   const [profileData, setProfileData] = useState({
-    profile_picture: currentUser.profile_picture,
-    cover_picture: currentUser.cover_picture,
-    bio: currentUser.bio || "",
-    location: currentUser.location || "",
-    website: currentUser.website || "",
+    profile_picture: currentUser?.profile_picture,
+    cover_picture: currentUser?.cover_picture,
+    bio: currentUser?.bio || "",
+    location: currentUser?.location || "",
+    website: currentUser?.website || "",
+    username: currentUser?.username || "",
   });
   const [selectedProfileFile, setSelectedProfileFile] = useState(null);
   const [selectedCoverFile, setSelectedCoverFile] = useState(null);
@@ -24,72 +25,7 @@ export default function EditProfilePage() {
   const profileFileRef = useRef(null);
   const coverFileRef = useRef(null);
 
-  const handleSubmit = async (e, pictureType) => {
-    e.preventDefault();
-    const formData = new FormData();
-
-    if (pictureType === "profile" && selectedProfileFile) {
-      formData.append("profile_picture", selectedProfileFile);
-    } else if (pictureType === "cover" && selectedCoverFile) {
-      formData.append("cover_picture", selectedCoverFile);
-    }
-
-    if (!pictureType) {
-      formData.append("bio", profileData.bio);
-      formData.append("location", profileData.location);
-      formData.append("website", profileData.website);
-    }
-
-    try {
-      const response = await api.patch("profiles/", formData, {
-        headers: { "Content-Type": "multipart/form-data" },
-      });
-
-      showMessage(
-        "success",
-        pictureType
-          ? `${
-              pictureType === "profile" ? "Profile" : "Cover"
-            } picture updated successfully`
-          : "Profile updated successfully"
-      );
-
-      setProfileData((prevData) => ({
-        ...prevData,
-        ...response.data,
-      }));
-
-      if (pictureType === "profile") {
-        setSelectedProfileFile(null);
-      } else if (pictureType === "cover") {
-        setSelectedCoverFile(null);
-      }
-    } catch (error) {
-      const errorMessage =
-        error.response?.data?.[`${pictureType}_picture`] ||
-        "Failed to update profile";
-      showMessage("error", errorMessage);
-    }
-  };
-
-  const handleCancel = (pictureType, fileInputRef) => {
-    if (pictureType === "profile") {
-      setSelectedProfileFile(null);
-      setProfileData((prevData) => ({
-        ...prevData,
-        profile_picture: currentUser.profile_picture,
-      }));
-    } else if (pictureType === "cover") {
-      setSelectedCoverFile(null);
-      setProfileData((prevData) => ({
-        ...prevData,
-        cover_picture: currentUser.cover_picture,
-      }));
-    }
-    fileInputRef.current.value = "";
-  };
-
-  const handleFileChange = (e, setFile, setData, key) => {
+  const handleFileChange = async (e, setFile, setData, key, pictureType) => {
     const file = e.target.files[0];
     if (file) {
       setFile(file);
@@ -97,15 +33,46 @@ export default function EditProfilePage() {
         ...prevData,
         [key]: URL.createObjectURL(file),
       }));
-    }
-  };
 
-  const handleProfileDetailsChange = (e) => {
-    const { name, value } = e.target;
-    setProfileData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+      const formData = new FormData();
+      formData.append(
+        pictureType === "profile" ? "profile_picture" : "cover_picture",
+        file
+      );
+
+      try {
+        const response = await api.patch(
+          `profiles/${currentUser?.id}/`,
+          formData,
+          {
+            headers: { "Content-Type": "multipart/form-data" },
+          }
+        );
+
+        showMessage(
+          "success",
+          `${
+            pictureType === "profile" ? "Profile" : "Cover"
+          } picture updated successfully`
+        );
+
+        setProfileData((prevData) => ({
+          ...prevData,
+          ...response.data,
+        }));
+
+        if (pictureType === "profile") {
+          setSelectedProfileFile(null);
+        } else if (pictureType === "cover") {
+          setSelectedCoverFile(null);
+        }
+      } catch (error) {
+        const errorMessage =
+          error.response?.data?.[`${pictureType}_picture`] ||
+          "Failed to update profile";
+        showMessage("error", errorMessage);
+      }
+    }
   };
 
   return (
@@ -116,39 +83,52 @@ export default function EditProfilePage() {
         setProfileData={setProfileData}
         selectedFile={selectedCoverFile}
         setSelectedFile={setSelectedCoverFile}
-        handleSubmit={(e) => handleSubmit(e, "cover")}
-        handleCancel={() => handleCancel("cover", coverFileRef)}
         fileRef={coverFileRef}
-        handleFileChange={handleFileChange}
+        handleFileChange={(e) =>
+          handleFileChange(
+            e,
+            setSelectedCoverFile,
+            setProfileData,
+            "cover_picture",
+            "cover"
+          )
+        }
         defaultPicture="/cover_default.png"
         label="Cover Picture"
       />
       <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 -mt-20 relative">
         <div className="bg-base-100 shadow-xl rounded-box p-6">
-          <div className="sm:flex sm:items-center sm:justify-between">
-            <div className="sm:flex sm:space-x-5">
+          <div className="sm:flex sm:space-x-5">
+            <div className="sm:flex sm:items-center sm:justify-between flex-shrink-0">
               <PictureForm
                 pictureType="profile"
                 profileData={profileData}
                 setProfileData={setProfileData}
                 selectedFile={selectedProfileFile}
                 setSelectedFile={setSelectedProfileFile}
-                handleSubmit={(e) => handleSubmit(e, "profile")}
-                handleCancel={() => handleCancel("profile", profileFileRef)}
                 fileRef={profileFileRef}
-                handleFileChange={handleFileChange}
+                handleFileChange={(e) =>
+                  handleFileChange(
+                    e,
+                    setSelectedProfileFile,
+                    setProfileData,
+                    "profile_picture",
+                    "profile"
+                  )
+                }
                 defaultPicture="/default_profile.png"
                 label="Profile Picture"
               />
-              <UserInfo profileData={profileData} />
             </div>
+
+            <UserInfo profileData={profileData} />
           </div>
         </div>
-        <div className="py-10">
+        <div className="mt-3">
           <ProfileDetailsForm
             profileData={profileData}
-            handleProfileDetailsChange={handleProfileDetailsChange}
-            handleSubmit={(e) => handleSubmit(e)}
+            setProfileData={setProfileData}
+            id={currentUser?.id}
           />
         </div>
       </div>
